9fcbf87fbdf0c5ff22fcd04b9a520f5705ee1697
Aishu <aishtt@gmail.com>
2016-05-31 21:34:53 -0400

    Initial Commit

---
+++ cdStoreMaven-master/cdStoreMaven-master/src/main/java/com/ottawau/cdstore/orderProcessing/MongoDAO/ICdMongoDAO.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * 
+ */
+
+package com.ottawau.cdstore.orderProcessing.MongoDAO;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Query;
+
+import com.ottawau.cdstore.orderProcessing.entity.Cd;
+
+public interface ICdMongoDAO extends IMongoGenericDAO <Cd, Serializable> {
+	
+	public String findAll() throws Exception;
+}
